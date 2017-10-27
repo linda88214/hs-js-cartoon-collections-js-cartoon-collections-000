33fb5c0ab3241fc33b603b20df7e902b03492d4f
Yerim Hu <yerimhu88@gmail.com>
2017-10-27 19:45:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
   var list = [];
-  for (var i = 0; i < dwarves.length; i++) {
+  for(var i = 0; i < dwarves.length; i++) {
     list.push(i+1 + ". " + dwarves[i] + " ")
   }
   return list.join("")
@@ -8,14 +8,14 @@ function dwarfRollCall(dwarves) {
 
 function summonCaptainPlanet(planeteerCalls){
   var planteer = [];
-  for (var i = 0; i < planeteerCalls.length; i++) {
+  for(var i = 0; i < planeteerCalls.length; i++) {
     planteer.push(planeteerCalls[i].toUpperCase + "!")
   }
   return planteer
 }
 
 function longPlaneteerCalls(words) {
-  for (var i = 0; i < words.length; i++)
+  var i = 0;
   if (words[i].length > 4) {
     return true
   } else {
@@ -24,25 +24,13 @@ function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
-  for (var i = 0; i < foods.length; i++)
-  if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
-    return foods[i]
-  }
-  return "no cheese!"
-}
-
-
-/*
-function findTheCheese (foods) {
-  var cheese = ["cheddar", "gouda", "camembert"];  // first created a cheese variable
-  for (var i = 0; i < foods.length; i++) {         // use for loop for foods with i
-    for (var j = 0; j < cheese.length; j++) {      // use for loop for cheese with j
-      if (foods[i] === cheese[j]) {                // if one of foods element is equal to one of foods element
-        return foods[i]                            // return that element inside the foods array
+  var cheese = ["cheddar", "gouda", "camembert"];
+  for (var i = 0; i < foods.length; i++) {
+    for( var j = 0; j < cheese.length; j++) {
+      if (foods[i] === cheese[j]) {
+        return foods[i]
       }
     }
   }
-  return "no cheese!"                              // if there is no matching with foods element and cheese element, return "no cheese!"
+  return "no cheese!"
 }
-
-*/
