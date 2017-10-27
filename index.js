58333391d85dfbbf0bf660788e8adb3b73d943c4
Yerim Hu <yerimhu88@gmail.com>
2017-10-27 19:46:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,13 +24,24 @@ function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
-  var cheese = ["cheddar", "gouda", "camembert"];
-  for (var i = 0; i < foods.length; i++) {
-    for( var j = 0; j < cheese.length; j++) {
-      if (foods[i] === cheese[j]) {
-        return foods[i]
-      }
-    }
+  for (var i = 0; i < foods.length; i++)
+  if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+    return foods[i]
   }
   return "no cheese!"
 }
+
+
+/*
+function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camembert"];  // first created a cheese variable
+  for (var i = 0; i < foods.length; i++) {         // use for loop for foods with i
+    for (var j = 0; j < cheese.length; j++) {      // use for loop for cheese with j
+      if (foods[i] === cheese[j]) {                // if one of foods element is equal to one of foods element
+        return foods[i]                            // return that element inside the foods array
+      }
+    }
+  }
+  return "no cheese!"                              // if there is no matching with foods element and cheese element, return "no cheese!"
+}
+*/
